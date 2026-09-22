ebcb7e482db1b21cf91dd858da87ce1c5f487e25
Izick7 <zickox491@gmail.com>
2026-09-22 09:54:58 +0100

    feat(products): add initial dummy products

--- src/data/products.js
+++ src/data/products.js
@@ -1,3 +1,16 @@
-const products = [];
+const products = [
+    {
+        id: 1,
+        name: "Laptop",
+        description: "Development laptop",
+        price: 450000
+    },
+    {
+        id: 2,
+        name: "Keyboard",
+        description: "USB mechanical keyboard",
+        price: 35000
+    }
+];
 
 module.exports = products;
